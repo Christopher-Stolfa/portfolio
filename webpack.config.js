b23b8cce103a4ad3ab9f374dfa92a19b3f15022e
Chribtoper <xcstolfax@gmail.com>
2022-09-20 15:55:11 -0700

    include jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ['babel-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
   },
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
 };
