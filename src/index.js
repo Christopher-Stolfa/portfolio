e6e85fef0aeb1f1b1fe9a2c8144f39fb86040412
Chribtoper <xcstolfax@gmail.com>
2022-09-22 13:21:49 -0700

    Accept hot loading

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from './App';
 
 // Renders the root react component
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Accept hot loading to reload when there are changes in code
+module.hot.accept();
