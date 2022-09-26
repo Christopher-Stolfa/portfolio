ed8d610e4986ee3245df64f8a72aeb4e11ef5c8e
Chribtoper <xcstolfax@gmail.com>
2022-09-26 15:35:40 -0700

    Never mind we're using arrows and turning this eslint rule off

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@
  * @component
  */
 
-const App = function () {
-  return (
-    <>
-      <header>Header</header>
-      <main>
-        <div>
-          <h1>Hello world</h1>
-          <p>Have a wonderful day</p>
-        </div>
-      </main>
-      <footer>Footer</footer>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <header>Header</header>
+    <main>
+      <div>
+        <h1>Hello world</h1>
+        <p>Have a wonderful day</p>
+      </div>
+    </main>
+    <footer>Footer</footer>
+  </>
+);
 
 export default App;
