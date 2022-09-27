e9bfd37fc057808b4e497123852ae7db00749ed0
Chribtoper <xcstolfax@gmail.com>
2022-09-26 17:23:47 -0700

    Use Dotenv for handling dynamic env variables

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,14 +1,13 @@
-const { DefinePlugin } = require('webpack');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 // Webkit dev env configuration
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   plugins: [
-    new DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
+    new Dotenv({
+      path: path.resolve(__dirname, '..', './.env.development'),
     }),
   ],
 };
