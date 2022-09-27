d8fd504695588c236cbcfa54fffdfe52fcca2c04
Chribtoper <xcstolfax@gmail.com>
2022-09-27 16:36:35 -0700

    Shorten comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 /**
- * This is the root React component, it gets rendered by ReactDOM.
+ * This is the root React component.
  * All
  *
  * @component
  */
 
 const App = () => (
-  <ChakraProvider>
-    <header>The Header</header>
-    <main>
-      <div>
-        <h1>Hello world</h1>
-        <p>Have a wonderful day</p>
-      </div>
-    </main>
-    <footer>The Footer</footer>
-  </ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider>
+      <header>The Header</header>
+      <main>
+        <div>
+          <h1>Hello world</h1>
+          <p>Have a wonderful day</p>
+        </div>
+      </main>
+      <footer>The Footer</footer>
+    </ChakraProvider>
+  </React.StrictMode>
 );
 
 export default App;
