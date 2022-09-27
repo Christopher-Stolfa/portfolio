ab87c20f0683afd38742ef1130bbb1c11734bbf2
Chribtoper <xcstolfax@gmail.com>
2022-09-27 16:31:38 -0700

    Use react 18 create root

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
 // Renders the root react component
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 // Accept hot loading to reload when there are changes in code
 module.hot.accept();
