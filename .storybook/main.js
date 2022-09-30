b844da8bc86ca7f913f9c421202c7cc4609eb0d9
Christopher Stolfa <xcstolfax@gmail.com>
2022-09-29 21:00:00 -0700

    * Install required storybook addons * Set up storybook config and remove unnecessary boilerplate from .storybook/main.js * Create storybook build and start scripts

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,19 +2,9 @@
  * Storybook configuration
  */
 module.exports = {
-  addons: ["@storybook/addon-essentials"],
-  babel: async (options) => ({
-    // Update your babel configuration here
-    ...options,
-  }),
+  addons: ["@storybook/addon-essentials", "@storybook/addon-postcss"],
   framework: "@storybook/react",
   stories: ["../src/**/*.stories.@(js|mdx)"],
-
-  webpackFinal: async (config, { configType }) => {
-    // Make whatever fine-grained changes you need
-    // Return the altered config
-    return config;
-  },
   features: {
     storyStoreV7: true,
   },
