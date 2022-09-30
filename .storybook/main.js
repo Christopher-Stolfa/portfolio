976943dcb89844a0cfba6828c15b0ca3634bea56
Christopher Stolfa <xcstolfax@gmail.com>
2022-09-29 20:43:59 -0700

    * Install storybook addons * Setup storybook config

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/**
+ * Storybook configuration
+ */
+module.exports = {
+  addons: ["@storybook/addon-essentials"],
+  babel: async (options) => ({
+    // Update your babel configuration here
+    ...options,
+  }),
+  framework: "@storybook/react",
+  stories: ["../src/**/*.stories.@(js|mdx)"],
+
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+    // Return the altered config
+    return config;
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
