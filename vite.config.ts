f38db0b368a2a595302fbe753743b2119b32a79a
Chris <xcstolfax@gmail.com>
2024-06-14 11:19:15 -0700

    adjust path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 // Convert import.meta.url to a file path
@@ -26,7 +26,7 @@ export default defineConfig({
     },
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables";`,
+        additionalData: `@import "@/styles/_variables";`,
       },
     },
   },
