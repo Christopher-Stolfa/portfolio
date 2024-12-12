3abb479726f7aa80df060cd506ecd3e537b23506
Christopher Stolfa <31984576+Christopher-Stolfa@users.noreply.github.com>
2024-12-11 19:07:23 -0700

    Dev (#9)
    * Change the nav link color and hover color
    
    * change color of navigation link when its active
    
    * Add a style that wraps all of the routed components
    
    * typing effect and styling
    
    * remove delay
    
    * rename vars
    
    * no longer importing styles
    
    * install node types
    
    * adjust path
    
    * install node types
    
    * create methods file
    
    * add global styles
    
    * add form types
    
    * Create a dummy contact form
    
    * move frontend files into its own folder
    
    * refactor
    
    * Add carets
    
    * remove carets
    
    * remove padding
    
    * use apostrophe
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor classname
    
    * decouple mixins
    
    * make async function
    
    * Apply typing effect function and add blurb section
    
    * update animation
    
    * add media query to lower h1 fontsize for mobile screen
    
    * create a utility called applyTypingEffect that sequentially renders each character of the input text
    
    * add paths to the typescript config
    
    * manually define port
    
    * re-install dependencies
    
    * Add avatar to homepage and apply typing effect to introduction. Add blurb
    
    * create fade in mixin
    
    * add global border radius for imgs
    
    * Add content to home page and about blurb. TODO: Fix github and linkedin links
    
    * Add node modules to git ignore
    
    * Feature/my work (#8)
    
    * Add content to my work section and display the work as a 2xn grid of cards.
    
    * Implement icons for links and technologies used.
    
    * Remove navigation, render everything as a single page app, improve biography language

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,29 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from './pages/home/index.vue';
-import About from './pages/about/index.vue';
-import Work from './pages/work/index.vue';
-import Contact from './pages/contact/index.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
-  {
-    path: '/work',
-    name: 'Work',
-    component: Work,
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: Contact,
   },
 ];
 
